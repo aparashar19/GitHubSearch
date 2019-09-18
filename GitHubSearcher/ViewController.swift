409500e0f3196eb7d08c7334a9b23c8a34211806
parashar <ayushiparashar89@gmail.com>
2019-09-18 10:37:35 -0400

    Initial Commit

---
+++ GitHubSearcher/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubSearcher
+//
+//  Created by Ayushi Parashar on 9/18/19.
+//  Copyright © 2019 Ayushi Parashar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
